c8f025693777983b009f1d1ffc066621bd53bc9e
jackielszhang <lapsun.j.zhang@gmail.com>
2025-02-13 16:21:56 +0200

    Initial Commit

---
+++ playground-test/playground_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  playground_testApp.swift
+//  playground-test
+//
+//  Created by Lap Sun Zhang on 2025/02/13.
+//
+
+import SwiftUI
+
+@main
+struct playground_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ playground-testTests/playground_testTests.swift
@@ -0,0 +1,17 @@
+//
+//  playground_testTests.swift
+//  playground-testTests
+//
+//  Created by Lap Sun Zhang on 2025/02/13.
+//
+
+import Testing
+@testable import playground_test
+
+struct playground_testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
